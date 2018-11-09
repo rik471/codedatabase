10db2ebce3553f13b40ee1a5f412de27375fca52
Rick.py <riksongleison471@gmail.com>
2018-11-09 18:45:58 -0200

    pacote codepress integrado com codedatabase

--- src/CodeDatabase/AbstractRepository.php
+++ src/CodeDatabase/AbstractRepository.php
@@ -6,7 +6,58 @@ use CodePress\CodeDatabase\Contracts\RepositoryInterface;
 
 abstract class AbstractRepository implements RepositoryInterface
 {
+    /**
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    public function __construct()
+    {
+        $this->makeModel();
+    }
+
+    public abstract function model();
+
+    public function makeModel()
+    {
+        $class = $this->model();
+        $this->model = new $class;
+        return $this->model;
+    }
 
 
+    public function all($columns = array('*'))
+    {
+        return $this->model->get($columns);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $model = $this->model->find($id);
+        $model->update($data);
+        return $model;
+
+    }
+
+    public function delete($id)
+    {
+        $model = $this->model->find($id);
+        return $model->delete();
+    }
+
+    public function find($id, $columns = array('*'))
+    {
+       return $this->model->findOrFail($id, $columns);
+    }
+
+    public function findBy($field, $value, $columns = array('*'))
+    {
+        return $this->model->where($field, '=', $value)->get($columns);
+    }
 
 }
--- tests/stub/Model/Category.php
+++ tests/stub/Model/Category.php
@@ -13,7 +13,7 @@ class Category extends Model
 
     protected $fillable = [
           'name',
-          'description'
+          'description',
     ];
 
 
